639ffe22de4cd5161673f573edecb8c85b777d43
Owen Busch <buschowen@gmail.com>
2024-11-14 14:31:34 -0700

    Character controller started

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,9 +1,80 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-namespace Character
+public class PlayerController : MonoBehaviour
 {
-    public class PlayerController : MonoBehaviour
+    [SerializeField] private CharacterController _controller;
+    private Vector3 _movement;
+
+    [SerializeField] private float gravitySpeed;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float sprintMultiplier;
+    private float _currentSpeed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float friction;
+
+    [SerializeField] private Vector3 playerVelocity;
+
+    [SerializeField] private float walkingFOV, sprintingFOV;
+    [SerializeField] private float fovAdjustSpeed;
+
+    private Camera _camera;
+
+    private bool _grounded;
+
+    private void Start()
     {
+        _controller ??= GetComponent<CharacterController>();
+        _camera ??= GetComponentInChildren<Camera>();
+        _currentSpeed = moveSpeed;
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+        CheckSpeed();
+    }
+
+    private void MovePlayer()
+    {
+        var xMovement = transform.right * (_movement.x * _currentSpeed);
+        var yMovement = transform.forward * (_movement.z * _currentSpeed);
+        var newMovement = xMovement + yMovement;
         
+        if (_controller.isGrounded && playerVelocity.y < 0) playerVelocity.y = 0;
+        var frictionMovement = Vector3.Lerp(playerVelocity, newMovement, friction * Time.deltaTime);
+        playerVelocity = new Vector3(frictionMovement.x, playerVelocity.y, frictionMovement.z);
+        playerVelocity.y += gravitySpeed * Time.deltaTime;
+        _controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    private void CheckSpeed()
+    {
+        _camera.fieldOfView = playerVelocity.magnitude switch
+        {
+            >= 7f => Mathf.Lerp(_camera.fieldOfView, sprintingFOV, fovAdjustSpeed * Time.deltaTime),
+            _ => Mathf.Lerp(_camera.fieldOfView, walkingFOV, fovAdjustSpeed * Time.deltaTime)
+        };
+    }
+
+    public void ToggleSprint(bool newSprint)
+    {
+        _currentSpeed = newSprint switch
+        {
+            true => moveSpeed * sprintMultiplier,
+            false => moveSpeed
+        };
+    }
+
+    public void Jump()
+    {
+        if (!_controller.isGrounded) return;
+        playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravitySpeed);
+    }
+
+    public void GetPlayerInput(Vector2 input)
+    {
+        _movement = new Vector3(input.x, 0, input.y);
     }
 }
--- Assets/Scripts/Input/PlayerMovementHandler.cs
+++ Assets/Scripts/Input/PlayerMovementHandler.cs
@@ -3,17 +3,31 @@ using UnityEngine;
 
 public class PlayerMovementHandler : MonoBehaviour
 {
+    private PlayerController _playerController;
+    private CameraController _camController;
     private CharacterControls _characterControls;
     
     private void OnEnable()
     {
+        _playerController = GetComponent<PlayerController>();
+        _camController = GetComponentInChildren<CameraController>();
+        
         if (_characterControls != null) return;
 
         _characterControls = new CharacterControls();
+
+        _characterControls.PlayerMovement.Move.performed += i => _playerController.GetPlayerInput(i.ReadValue<Vector2>());
+        _characterControls.PlayerActions.Jump.performed += i => _playerController.Jump();
+        _characterControls.PlayerActions.Sprint.started += i => _playerController.ToggleSprint(true);
+        _characterControls.PlayerActions.Sprint.canceled += i => _playerController.ToggleSprint(false);
+
+        _characterControls.PlayerMovement.Look.performed += i => _camController.GetCameraInput(i.ReadValue<Vector2>());
+        
+        _characterControls.Enable();
     }
 
     private void OnDestroy()
     {
-        Destroy(this);
+        _characterControls.Disable(); 
     }
 }
