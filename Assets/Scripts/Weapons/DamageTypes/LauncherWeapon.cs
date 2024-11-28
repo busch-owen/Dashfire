da1b8a0834599e6cbb8478886e523603bec1c400
Owen Busch <buschowen@gmail.com>
2024-11-28 11:32:55 -0700

    Minor refactoring

--- Assets/Scripts/Weapons/DamageTypes/LauncherWeapon.cs
+++ Assets/Scripts/Weapons/DamageTypes/LauncherWeapon.cs
@@ -3,7 +3,5 @@ using UnityEngine;
 public class LauncherWeapon : IWeaponDamage
 {
     private PlayerController _player;
-
-    protected PoolManager LocalPoolManager;
     private LayerMask _playerMask;
 }
--- Assets/Scripts/Weapons/LauncherWeaponBase.cs
+++ Assets/Scripts/Weapons/LauncherWeaponBase.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class LauncherWeaponBase : WeaponBase
 {
     [SerializeField] private LauncherWeaponSO launcherWeaponSO;
-
     private Transform _cameraTransform;
     
     public override void UseWeapon()
-    {
+    { 
         _cameraTransform ??= OwnerObject.GetComponentInChildren<Camera>().transform;
-       if(!CanFire || Reloading || CurrentAmmo <= 0) return;
-       OwnerObject?.AddForceInVector(-_cameraTransform.forward * launcherWeaponSO.LaunchForce);
-       base.UseWeapon();
+        if(!CanFire || Reloading || CurrentAmmo <= 0) return;
+        OwnerObject?.AddForceInVector(-_cameraTransform.forward * launcherWeaponSO.LaunchForce);
+        base.UseWeapon();
     }
 }
