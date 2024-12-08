f536539a5bb7e7a8b4e271a6a9eda799e3d9fa41
busch-owen <buschowen@gmail.com>
2024-12-08 16:32:56 -0700

    Did a quick null check on the stored stats

--- Assets/ScoreSaver.cs
+++ Assets/ScoreSaver.cs
@@ -34,6 +34,11 @@ public class ScoreSaver : NetworkBehaviour
             NetworkManager.SpawnManager.SpawnedObjects.TryGetValue(playerObjId, out var client);
             if (client == null) return;
             var currentData = client.GetComponent<PlayerData>();
+            if (_storedData.Count == 0)
+            {
+                Debug.Log("no stored data, no stats loaded");
+                return;
+            }
             currentData.PlayerWins = _storedData.Find(e => e == client.GetComponent<PlayerData>()).PlayerWins;
             Debug.Log("Loaded data for player: " + currentData.gameObject.name);
         }
