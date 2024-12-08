85f29150ee9b5a8a775ab9774eaeea0833d49140
busch-owen <buschowen@gmail.com>
2024-12-08 16:39:42 -0700

    converted stored data to a dictionary

--- Assets/ScoreSaver.cs
+++ Assets/ScoreSaver.cs
@@ -1,14 +1,12 @@
-using System;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class ScoreSaver : NetworkBehaviour
 {
     public static ScoreSaver Instance;
-    
-    private List<PlayerData> _storedData = new();
+
+    private Dictionary<ulong, PlayerData> _storedData = new();
 
     private void Awake()
     {
@@ -39,7 +37,10 @@ public class ScoreSaver : NetworkBehaviour
                 Debug.Log("no stored data, no stats loaded");
                 return;
             }
-            //currentData.PlayerWins = _storedData.Find(e => e == client.GetComponent<PlayerData>()).PlayerWins;
+
+            _storedData.TryGetValue(client.OwnerClientId, out var newData);
+            if (!newData) return;
+            currentData.PlayerWins.Value = newData.PlayerWins.Value;
             Debug.Log("Loaded data for player: " + currentData.gameObject.name);
         }
     }
@@ -53,7 +54,7 @@ public class ScoreSaver : NetworkBehaviour
             NetworkManager.ConnectedClients.TryGetValue(id, out var client);
             if (client == null) return;
             var playerObj = client.PlayerObject;
-            _storedData?.Add(playerObj.GetComponent<PlayerData>());
+            _storedData?.Add(playerObj.OwnerClientId, playerObj.GetComponent<PlayerData>());
             playerObj.GetComponent<PlayerData>().ClearValues();
             Debug.Log("Stored data for player" + playerObj.gameObject.name);
         }
