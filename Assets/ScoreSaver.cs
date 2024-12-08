97ecf9ebdb0af311a8afe2004fca32f096dd3dc9
busch-owen <buschowen@gmail.com>
2024-12-08 15:36:35 -0700

    Was trying to find players using the wrong IDs

--- Assets/ScoreSaver.cs
+++ Assets/ScoreSaver.cs
@@ -30,17 +30,17 @@ public class ScoreSaver : NetworkBehaviour
 
     private void ApplyScoresToPlayers(string sceneName, LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientstTimedOut)
     {
-        if (IsHost && SceneManager.GetActiveScene().name == sceneName)
+        if (!IsHost || SceneManager.GetActiveScene().name != sceneName) return;
+        
+        foreach (var id in clientsCompleted)
         {
-            foreach (var id in clientsCompleted)
-            {
-                NetworkManager.SpawnManager.SpawnedObjects.TryGetValue(id, out var client);
-                if (!client) return;
-                var currentData = client.GetComponent<PlayerData>();
-                currentData.PlayerWins = _storedData.Find(client.GetComponent<Predicate<PlayerData>>()).PlayerWins;
-                Debug.Log("Loaded data for player: " + currentData.gameObject.name);
+            NetworkManager.ConnectedClients.TryGetValue(id, out var client);
+            if (client == null) return;
+            var playerObj = client.PlayerObject;
+            var currentData = playerObj.GetComponent<PlayerData>();
+            currentData.PlayerWins = _storedData.Find(playerObj.GetComponent<Predicate<PlayerData>>()).PlayerWins;
+            Debug.Log("Loaded data for player: " + currentData.gameObject.name);
                 
-            }
         }
     }
 
@@ -49,11 +49,12 @@ public class ScoreSaver : NetworkBehaviour
         _storedData?.Clear();
         foreach (var id in clientsCompleted)
         {
-            NetworkManager.SpawnManager.SpawnedObjects.TryGetValue(id, out var client);
-            if (!client) return;
-            _storedData?.Add(client.GetComponent<PlayerData>());
-            client.GetComponent<PlayerData>().ClearValues();
-            Debug.Log("Stored data for player" + client.gameObject.name);
+            NetworkManager.ConnectedClients.TryGetValue(id, out var client);
+            if (client == null) return;
+            var playerObj = client.PlayerObject;
+            _storedData?.Add(playerObj.GetComponent<PlayerData>());
+            playerObj.GetComponent<PlayerData>().ClearValues();
+            Debug.Log("Stored data for player" + playerObj.gameObject.name);
         }
     }
 }
