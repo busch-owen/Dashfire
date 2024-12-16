eaf19bfed6b7f718e1c87d6b7961756e04f9c68f
busch-owen <buschowen@gmail.com>
2024-12-16 15:25:09 -0700

    Undoing that, because it didn't work

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -371,6 +371,19 @@ public class PlayerController : NetworkBehaviour
 
     public void TakeDamage(float damageToDeal, ulong dealerClientId, ulong dealerObjId)
     {
+        //Damage indicator logic
+        NetworkManager.SpawnManager.SpawnedObjects.TryGetValue(dealerObjId, out var castingPlayer);
+        if (!castingPlayer) return;
+        var angle = Mathf.Atan2(castingPlayer.transform.position.z - transform.position.z,
+            castingPlayer.transform.position.x - transform.position.x);
+        if (castingPlayer.NetworkObjectId == NetworkObjectId)
+        {
+            angle = -90f;
+        }
+        _canvasHandler.StopAllCoroutines();
+        StartCoroutine(_canvasHandler.ShowDamageIndicator(angle));
+        Debug.Log("I've been hit!");
+        
         //Damage math logic
         var armorDamage = damageToDeal * armorDamping;
         var playerDamage = CurrentArmor > 0 ? damageToDeal - armorDamage : damageToDeal;
